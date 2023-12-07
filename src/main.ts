a41a7af2e358d50ea4fbb256f3e3caea35eee03a
Johan Frick <johan.frick@gmail.com>
2023-12-07 20:53:04 +0100

    fix: blank card
    Too much removed during the commit "fix: remove unsupported section cards"

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
+import { Player } from './sections/player';
 import { Card } from './card';
+import { Grouping } from './sections/grouping';
+import { Groups } from './sections/groups';
+import { MediaBrowser } from './sections/media-browser';
 import './sections/volumes';
 
 const name = (type?: string) => `Sonos${type ? ` (${type})` : ''}`;
@@ -12,3 +16,7 @@ window.customCards.push({
 });
 
 customElements.define('sonos-card', Card);
+customElements.define('sonos-grouping', Grouping);
+customElements.define('sonos-groups', Groups);
+customElements.define('sonos-media-browser', MediaBrowser);
+customElements.define('sonos-player', Player);
