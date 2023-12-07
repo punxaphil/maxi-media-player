632542cd8f7be0dbef951909cd377052d88a5982
Johan Frick <johan.frick@gmail.com>
2023-12-07 12:52:32 +0100

    fix: remove unsupported section cards

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,14 @@
-import { Player } from './sections/player';
 import { Card } from './card';
-import { Grouping } from './sections/grouping';
-import { Groups } from './sections/groups';
-import { MediaBrowser } from './sections/media-browser';
 import './sections/volumes';
 
 const name = (type?: string) => `Sonos${type ? ` (${type})` : ''}`;
 const desc = (type?: string) => `Media player for your Sonos speakers${type ? ` (${type})` : ''}`;
 
-window.customCards.push(
-  {
-    type: 'sonos-card',
-    name: name(),
-    description: desc(),
-    preview: true,
-  },
-  {
-    type: 'sonos-grouping',
-    name: name('Grouping section'),
-    description: desc('Grouping section'),
-    preview: true,
-  },
-  {
-    type: 'sonos-groups',
-    name: name('Groups section'),
-    description: desc('Groups section'),
-    preview: true,
-  },
-  {
-    type: 'sonos-media-browser',
-    name: name('Media Browser section'),
-    description: desc('Media Browser section'),
-    preview: true,
-  },
-  {
-    type: 'sonos-player',
-    name: name('Player section'),
-    description: 'Media player for your Sonos speakers (Player section)',
-    preview: true,
-  },
-);
+window.customCards.push({
+  type: 'sonos-card',
+  name: name(),
+  description: desc(),
+  preview: true,
+});
 
 customElements.define('sonos-card', Card);
-customElements.define('sonos-grouping', Grouping);
-customElements.define('sonos-groups', Groups);
-customElements.define('sonos-media-browser', MediaBrowser);
-customElements.define('sonos-player', Player);
