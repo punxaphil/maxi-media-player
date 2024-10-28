03b50be4aadfea485990d619f1ef9c018e42b528
Johan Frick <johan.frick@gmail.com>
2024-10-28 22:07:42 +0100

    fix: remove sourcemaps error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    minify: false,
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'src/main.ts',
+
+      output: {
+        entryFileNames: 'maxi-media-player.js',
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
